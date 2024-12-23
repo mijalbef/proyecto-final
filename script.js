0fe015c53972d6a6fd8de4d0e5aec95fbb741a2e
MilenaManosalvas <milenamanosalvasv@gmail.com>
2024-12-23 04:41:25 +0100

    js oficial

--- script.js
+++ script.js
@@ -4,6 +4,44 @@ document.addEventListener("DOMContentLoaded", function () {
 
     toggler.addEventListener("click", function () {
         collapse.classList.toggle("show");
-        toggler.classList.toggle("collapsed");
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const options = {
+        strings: ["FRESH FRAMES", "TUS NUEVOS", "ARTISTAS FAVORITOS"],
+        typeSpeed: 20, // Aumenta la velocidad de tipeo
+        backSpeed: 30, // Velocidad al borrar
+        backDelay: 300, // Retraso entre cada palabra
+        startDelay: 500, // Retraso inicial de medio segundo
+        loop: false, // No repetir la animación
+        showCursor: true, // Mostrar el cursor durante el efecto
+        cursorChar: "|", // Caracter del cursor (opcional)
+        onComplete: () => {
+            // Mantener el texto completo visible
+            document.getElementById("typed-text").innerHTML = `
+                <span style="font-family: 'Montserrat', sans-serif;">FRESH FRAMES</span><br>
+                <span style="font-family: 'Montserrat', sans-serif;">TUS NUEVOS</span><br>
+                <span style="font-family: 'Montserrat', sans-serif;">ARTISTAS FAVORITOS</span>
+            `;
+        }
+    };
+
+    // Inicia Typed.js
+    setTimeout(() => {
+        new Typed("#typed-text", options);
+    }, 300); // Reducción del retraso adicional para una transición más rápida
+});
+
+
+// CTITULO CARROUSEL
+
+AOS.init({
+    duration: 1000, // Duración de la animación en milisegundos
+    easing: 'ease-in-out', // Suavizado
+    once: true // Ejecutar animación solo una vez
+});
+
+
+// 3DDD
+
